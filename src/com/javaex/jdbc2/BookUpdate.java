0dcd4aad89a9feb67ad704c3a34529b43f402282
whjkh <whjkh@DESKTOP-O9T8HTK>
2020-12-28 18:16:24 +0900

    2020. 12. 28 수업과제 #jdbc

---
+++ src/com/javaex/jdbc2/BookUpdate.java
@@ -0,0 +1,67 @@
+package com.javaex.jdbc2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BookUpdate {
+
+	public static void main(String[] args) {
+
+		// 0. import java.sql.*;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		// ResultSet rs = null;
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2. Connection 얻어오기
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// UPDATE book SET title = '해리포터 시리즈' WHERE book_id = 9;
+			String query = "";
+			query += " update book ";
+			query += " SET title = ? ";
+			query += " WHERE book_id = ? ";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, "해리포터 시리즈");
+			pstmt.setInt(2, 9);
+
+			int count = pstmt.executeUpdate(); // update
+
+			// rs = pstmt.executeQuery(); // select
+
+			// 4.결과처리
+			System.out.println("[" + count + "건이 수정되었습니다.]");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			// 5. 자원정리
+			try {
+				/*
+				 * if (rs != null) { rs.close(); }
+				 */
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+
+		}
+
+	}
+
+}
